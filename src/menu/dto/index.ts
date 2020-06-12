d274802a6fdb442e583190851d6656beb171bef5
Riantis <info@weemhoff.de>
2020-06-12 14:03:23 +0200

    last

--- src/menu/dto/index.ts
+++ src/menu/dto/index.ts
@@ -1,3 +1,2 @@
 export { CreateMenuDto } from './create-menu.dto';
 export { CreateMenuEntryDto } from './create-menuentry.dto';
-export { CreateMenuEntryPointDto } from './create-menuentrypoint.dto';
